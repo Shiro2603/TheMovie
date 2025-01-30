bc8a5631877de8468cc31fa201a5b12c0f80bc81
Tima <tsuahnov26@gmail.com>
2025-01-30 18:49:36 +0500

    Началь верстать начальный экран

--- app/src/main/java/ui/main/MainActivity.kt
+++ app/src/main/java/ui/main/MainActivity.kt
@@ -5,24 +5,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.themovie.R
+import com.example.themovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding : ActivityMainBinding get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
         val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
+
+//        navController.addOnDestinationChangedListener { _, destination, _ ->
+//            when(destination.id) { }
+//        }
+
     }
+
+    private fun hideBottomNavigation() {
+        binding.bottomNavigation.isVisible = false
+    }
+
+    private fun showBottomNavigation() {
+        binding.bottomNavigation.isVisible = true
+    }
+
 
 
 
